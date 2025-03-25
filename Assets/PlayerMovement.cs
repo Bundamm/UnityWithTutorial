64655d7c29916567435997829e482b9dcd535ab2
bundamm <tomcio.szewa@gmail.com>
2025-03-25 20:02:05 +0100

    First Commit
    Played around with tiles and player movement using the first two guides

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public PlayerController playerController;
+
+    float horizontalMove = 0f;
+    
+    public float runSpeed = 40f;
+    bool jump = false;
+    bool crouch = false;
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        } else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        playerController.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
